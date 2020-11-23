257792edeb716159f8937406f1662951fc4bf340
naveed-dev30 <naveed.dev30@gmail.com>
2020-11-23 14:02:15 +0500

    asd

--- app/src/main/java/com/example/final_2020_cs/MainActivity.java
+++ app/src/main/java/com/example/final_2020_cs/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.final_2020_cs;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actiondrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.navigationview);
+        drawerLayout = findViewById(R.id.drawer);
+
+        actiondrawer = new ActionBarDrawerToggle(
+                this,drawerLayout,R.string.app_name,R.string.app_name
+        );
+
+        drawerLayout.addDrawerListener(actiondrawer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 }
